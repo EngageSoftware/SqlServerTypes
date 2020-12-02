1eb45b6a6aa09fb90b2041f195fa66c517f3a1aa
Brian Dukes <bdukes@engagesoftware.com>
2020-12-02 13:42:45 -0600

    Clean up build script

--- build/Tasks/Package.cs
+++ build/Tasks/Package.cs
@@ -5,6 +5,7 @@ namespace Engage.Dnn.SqlServerTypes.Build.Tasks
     using Cake.Common.Build;
     using Cake.Common.Diagnostics;
     using Cake.Common.IO;
+    using Cake.Common.IO.Paths;
     using Cake.Common.Solution.Project.Properties;
     using Cake.Common.Tools.MSBuild;
     using Cake.Common.Xml;
@@ -16,57 +17,87 @@ namespace Engage.Dnn.SqlServerTypes.Build.Tasks
     {
         public override void Run(Context context)
         {
+            var version = ReadVersion(context);
+            SetAssemblyInfoVersion(context, version);
+            CleanAndBuild(context);
+
             var dist = context.Directory("dist");
-            var pkg = dist.Path.Combine("pkg");
-            var pkgBin = pkg.Combine("bin");
-            var packagesConfig = context.File("src/packages.config");
-            var assemblyInfoPath = context.File("src/Properties/AssemblyInfo.cs");
+            var (pkg, pkgBin) = CreateAndCleanDist(context, dist);
 
-            var version = context.XmlPeek(packagesConfig, "//package[@id=\"Microsoft.SqlServer.Types\"]/@version");
-            context.Information($"Microsoft.SqlServer.Types NuGet package version {version}");
-
-            var assemblyInfo = context.ParseAssemblyInfo(assemblyInfoPath);
-            context.CreateAssemblyInfo(assemblyInfoPath, new AssemblyInfoSettings
-                                                             {
-                                                                 Version = version,
-                                                                 FileVersion = version,
-                                                                 InformationalVersion = version,
-                                                                 Company = assemblyInfo.Company,
-                                                                 Configuration = assemblyInfo.Configuration,
-                                                                 CLSCompliant = assemblyInfo.ClsCompliant,
-                                                                 Copyright = assemblyInfo.Copyright,
-                                                                 Description = assemblyInfo.Description,
-                                                                 Guid = string.IsNullOrWhiteSpace(assemblyInfo.Guid) ? null : assemblyInfo.Guid,
-                                                                 Product = assemblyInfo.Product,
-                                                                 Title = assemblyInfo.Title,
-                                                                 Trademark = assemblyInfo.Trademark,
-                                                                 ComVisible = assemblyInfo.ComVisible,
-                                                                 InternalsVisibleTo = assemblyInfo.InternalsVisibleTo,
-                                                             });
-
-            var settings =
-                new MSBuildSettings { Configuration = "Release", MaxCpuCount = 0, Verbosity = Verbosity.Minimal, }
-                    .WithRestore()
-                    .WithTarget("clean")
-                    .WithTarget("build");
-            context.MSBuild("Engage.Dnn.SqlServerTypes.sln", settings);
-
-            context.CreateDirectory(dist);
-            context.CleanDirectory(dist);
-            context.CreateDirectory(pkgBin);
-
-            context.CopyFiles("src/bin/Release/**/*.dll", pkgBin, preserveFolderStructure: true);
             var manifest = pkg.CombineWithFilePath("Engage.Dnn.SqlServerTypes.dnn");
-            context.CopyFile("src/bin/Release/Engage.Dnn.SqlServerTypes.dnn", manifest);
-
-            context.XmlPoke(manifest, "//package/@version", version);
-            context.XmlPoke(manifest, "//assembly[name/text()=\"Engage.Dnn.SqlServerTypes.dll\"]/version", version);
-            context.XmlPoke(manifest, "//assembly[name/text()=\"Microsoft.SqlServer.Types.dll\"]/version", version);
-            context.XmlPoke(manifest, "//assembly[name/text()=\"SqlServerSpatial140.dll\"]/version", version);
+            CopyPackageFiles(context, pkgBin, manifest);
+            SetManifestVersions(context, manifest, version);
 
             context.Zip(pkg, dist.Path.CombineWithFilePath($"Engage.Dnn.SqlServerTypes_{version}_Install.zip"));
 
             context.DeleteDirectory(pkg, new DeleteDirectorySettings { Force = true, Recursive = true, });
+        }
+
+        private static (DirectoryPath pkg, DirectoryPath pkgBin) CreateAndCleanDist(Context context, ConvertableDirectoryPath dist)
+        {
+            context.CreateDirectory(dist);
+            context.CleanDirectory(dist);
+            var pkg = dist.Path.Combine("pkg");
+            var pkgBin = pkg.Combine("bin");
+            context.CreateDirectory(pkgBin);
+
+            return (pkg, pkgBin);
+        }
+
+        private static string ReadVersion(Context context)
+        {
+            var packagesConfig = context.File("src/packages.config");
+            var version = context.XmlPeek(packagesConfig, "//package[@id=\"Microsoft.SqlServer.Types\"]/@version");
+            context.Information($"Microsoft.SqlServer.Types NuGet package version {version}");
+            return version;
+        }
+
+        private static void CopyPackageFiles(Context context, DirectoryPath pkgBin, FilePath manifest)
+        {
+            context.CopyFiles("src/bin/Release/**/*.dll", pkgBin, preserveFolderStructure: true);
+            context.CopyFile("src/bin/Release/Engage.Dnn.SqlServerTypes.dnn", manifest);
+        }
+
+        private static void SetManifestVersions(Context context, FilePath manifest, string version)
+        {
+            context.XmlPoke(manifest, "//package/@version", version);
+            context.XmlPoke(manifest, "//assembly[name/text()=\"Engage.Dnn.SqlServerTypes.dll\"]/version", version);
+            context.XmlPoke(manifest, "//assembly[name/text()=\"Microsoft.SqlServer.Types.dll\"]/version", version);
+            context.XmlPoke(manifest, "//assembly[name/text()=\"SqlServerSpatial140.dll\"]/version", version);
+        }
+
+        private static void CleanAndBuild(Context context)
+        {
+            var settings = new MSBuildSettings { Configuration = "Release", MaxCpuCount = 0, Verbosity = Verbosity.Minimal, }
+                .WithRestore()
+                .WithTarget("clean")
+                .WithTarget("build");
+            context.MSBuild("Engage.Dnn.SqlServerTypes.sln", settings);
+        }
+
+        private static void SetAssemblyInfoVersion(Context context, string version)
+        {
+            var assemblyInfoPath = context.File("src/Properties/AssemblyInfo.cs");
+            var assemblyInfo = context.ParseAssemblyInfo(assemblyInfoPath);
+            context.CreateAssemblyInfo(
+                assemblyInfoPath,
+                new AssemblyInfoSettings
+                    {
+                        Version = version,
+                        FileVersion = version,
+                        InformationalVersion = version,
+                        Company = assemblyInfo.Company,
+                        Configuration = assemblyInfo.Configuration,
+                        CLSCompliant = assemblyInfo.ClsCompliant,
+                        Copyright = assemblyInfo.Copyright,
+                        Description = assemblyInfo.Description,
+                        Guid = string.IsNullOrWhiteSpace(assemblyInfo.Guid) ? null : assemblyInfo.Guid,
+                        Product = assemblyInfo.Product,
+                        Title = assemblyInfo.Title,
+                        Trademark = assemblyInfo.Trademark,
+                        ComVisible = assemblyInfo.ComVisible,
+                        InternalsVisibleTo = assemblyInfo.InternalsVisibleTo,
+                    });
         }
     }
 }
