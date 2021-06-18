af893919f7e9b613a74f7c0f8887eb52c99b1a95
Brian Dukes <bdukes@engagesoftware.com>
2021-06-18 13:05:38 -0500

    Update Cake

--- build/Lifetime.cs
+++
@@ -1,8 +0,0 @@
-namespace Engage.Dnn.SqlServerTypes.Build
-{
-    using Cake.Frosting;
-
-    public sealed class Lifetime : FrostingLifetime<Context>
-    {
-    }
-}
--- build/Program.cs
+++ build/Program.cs
@@ -1,35 +1,18 @@
 namespace Engage.Dnn.SqlServerTypes.Build
 {
     using System;
-    using System.Collections.Generic;
 
-    using Cake.Core.Configuration;
     using Cake.Frosting;
-    using Cake.NuGet;
 
-    public class Program : IFrostingStartup
+    public class Program
     {
         public static int Main(string[] args)
         {
-            // Create the host.
-            var host = new CakeHostBuilder().WithArguments(args)
-                .UseStartup<Program>()
-                .Build();
-
-            // Run the host.
-            return host.Run();
-        }
-
-        public void Configure(ICakeServices services)
-        {
-            services.UseContext<Context>();
-            services.UseLifetime<Lifetime>();
-            services.UseWorkingDirectory("..");
-            
-            var module = new NuGetModule(new CakeConfiguration(new Dictionary<string, string>()));
-            module.Register(services);
-
-            services.UseTool(new Uri("nuget:?package=NuGet.CommandLine&version=5.8.0"));
+            return new CakeHost()
+                .UseContext<Context>()
+                .UseWorkingDirectory("..")
+                .InstallTool(new Uri("nuget:?package=NuGet.CommandLine&version=5.9.1"))
+                .Run(args);
         }
     }
 }
